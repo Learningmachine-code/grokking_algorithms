7aa6daed0c2403e39cd6008e3a64e110955329d5
Learningmachine-code <learningmachines@outlook.com>
2024-10-26 21:02:27 +0800

    2024-10-26

--- 02_selection_sort/rust/01_selection_sort/src/main.rs
+++ 02_selection_sort/rust/01_selection_sort/src/main.rs
@@ -7,7 +7,7 @@ fn swap<T: Copy>(list: &mut [T], index_a: usize, index_b: usize) {
 
 fn selection_sort<T: PartialOrd + Copy>(list: &mut [T]) {
     for i in 0..list.len() {
-        for j in (i+1)..list.len() {
+        for j in (i + 1)..list.len() {
             if list[j] > list[i] {
                 swap(list, i, j);
             }
@@ -28,8 +28,8 @@ mod test {
 
     #[test]
     fn sort_unsigned_list() {
-        let mut list : [u8; 7] = [156, 141, 35, 94, 88, 61, 111];
-        let list_sorted : [u8; 7] = [156, 141, 111, 94, 88, 61, 35];
+        let mut list: [u8; 7] = [156, 141, 35, 94, 88, 61, 111];
+        let list_sorted: [u8; 7] = [156, 141, 111, 94, 88, 61, 35];
         selection_sort(&mut list);
 
         assert_eq!(list, list_sorted);
@@ -37,8 +37,8 @@ mod test {
 
     #[test]
     fn sort_signed_list() {
-        let mut list : [i32; 10] = [75, 85, -26, 61, 20, -40, -72, 30, -27, 58];
-        let list_sorted : [i32; 10] = [85, 75, 61, 58, 30, 20, -26, -27, -40, -72];
+        let mut list: [i32; 10] = [75, 85, -26, 61, 20, -40, -72, 30, -27, 58];
+        let list_sorted: [i32; 10] = [85, 75, 61, 58, 30, 20, -26, -27, -40, -72];
         selection_sort(&mut list);
 
         assert_eq!(list, list_sorted);
@@ -46,11 +46,23 @@ mod test {
 
     #[test]
     fn sort_strings() {
-        let mut list : [&str; 7] = ["Radiohead", "Kishore Kumar", "The Black Keys",
-            "Neutral Milk Hotel", "Beck", "The Strokes", "Wilco"
+        let mut list: [&str; 7] = [
+            "Radiohead",
+            "Kishore Kumar",
+            "The Black Keys",
+            "Neutral Milk Hotel",
+            "Beck",
+            "The Strokes",
+            "Wilco",
         ];
-        let list_sorted : [&str; 7] = ["Wilco", "The Strokes", "The Black Keys", 
-            "Radiohead", "Neutral Milk Hotel", "Kishore Kumar", "Beck"
+        let list_sorted: [&str; 7] = [
+            "Wilco",
+            "The Strokes",
+            "The Black Keys",
+            "Radiohead",
+            "Neutral Milk Hotel",
+            "Kishore Kumar",
+            "Beck",
         ];
         selection_sort(&mut list);
 
@@ -59,10 +71,11 @@ mod test {
 
     #[test]
     fn sorts_an_empty_list() {
-        let mut list : [u8; 0] = [];
-        let list_sorted : [u8; 0] = [];
+        let mut list: [u8; 0] = [];
+        let list_sorted: [u8; 0] = [];
         selection_sort(&mut list);
 
         assert_eq!(list, list_sorted);
     }
 }
+
--- my-solution/convert/target/debug/deps/convert-87e55483f2e02823.d
+++ my-solution/convert/target/debug/deps/convert-87e55483f2e02823.d
@@ -1,10 +1,9 @@
-/home/a2580ti/code/learn-code/grokking_algorithms/my-solution/convert/target/debug/deps/libconvert-87e55483f2e02823.rmeta: src/main.rs src/exercise/mod.rs src/exercise/q2.rs Cargo.toml
+/home/a2580ti/code/learn-code/grokking_algorithms/my-solution/convert/target/debug/deps/libconvert-87e55483f2e02823.rmeta: src/main.rs src/exercise/mod.rs Cargo.toml
 
-/home/a2580ti/code/learn-code/grokking_algorithms/my-solution/convert/target/debug/deps/convert-87e55483f2e02823.d: src/main.rs src/exercise/mod.rs src/exercise/q2.rs Cargo.toml
+/home/a2580ti/code/learn-code/grokking_algorithms/my-solution/convert/target/debug/deps/convert-87e55483f2e02823.d: src/main.rs src/exercise/mod.rs Cargo.toml
 
 src/main.rs:
 src/exercise/mod.rs:
-src/exercise/q2.rs:
 Cargo.toml:
 
 # env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
