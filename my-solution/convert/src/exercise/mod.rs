a7da30499b569a20f30564ed5d4172629d47a95f
Learningmachine-code <learningmachines@outlook.com>
2024-11-02 18:55:16 +0800

    2024-11-02

--- my-solution/convert/src/exercise/mod.rs
+++ my-solution/convert/src/exercise/mod.rs
@@ -1,3 +1,3 @@
 // pub mod example_1;
 // pub mod q1;
-// pub mod q2;
+pub mod q2;
--- my-solution/convert/src/main.rs
+++ my-solution/convert/src/main.rs
@@ -2,7 +2,6 @@ mod exercise;
 // run test:
 // cargo test -- --nocapture
 fn main() {
-    let s: Vec<i8> = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let item: i8 = 8;
+    let s: Vec<i8> = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]   let item: i8 = 8;
     exercise::q1::binary_search(s, item);
 }
